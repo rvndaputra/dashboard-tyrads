b01d9185c40da43a9ef4fb1493dec951d897ba97
Ravin <rvnda.putra@gmail.com>
2024-02-15 16:34:26 +0700

    chore: disable select cardholder image

--- src/app/presentations/Cardholder.tsx
+++ src/app/presentations/Cardholder.tsx
@@ -6,14 +6,15 @@ const Cardholder = () => {
     <div className="text-md text-white font-semibold tracking-wide">
       <div className="relative h-64 w-96 bg-gradient-to-r from-indigo-900 via-violet-600 to-purple-900 rounded-md">
         <Image
-          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 select-none"
           src="/assets/images/inky-credit-card.png"
           width={350}
           height={350}
           alt="card img"
+          draggable={false}
         />
         <Image
-          className="absolute top-5 right-5"
+          className="absolute top-5 right-5 select-none"
           src="/assets/images/visa-logo-white.webp"
           width={50}
           height={50}
